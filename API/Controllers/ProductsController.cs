27d923bb36ab23505c2775002224d5f4a645ccc0
Sharpsharky <maciej.jasielewicz@gmail.com>
2023-08-31 17:22:27 +0200

    Added SQLite and Created and created a database

---
+++ API/Controllers/ProductsController.cs
@@ -0,0 +1,21 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace API.Controllers
+{
+    [ApiController]
+    [Route("api/[controller]")]
+    public class ProductsController : Controller
+    {
+        [HttpGet]
+        public string GetProducts()
+        {
+            return "my list of products";
+        }
+
+        [HttpGet("{id}")]
+        public string GetProduct(int id)
+        {
+            return "my product";
+        }
+    }
+}
