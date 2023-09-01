a1321c3f280afe0e33716d760e0a94b0c988d48b
Sharpsharky <maciej.jasielewicz@gmail.com>
2023-09-01 11:55:10 +0200

    Added Infrastructure and Data projects

--- API/Controllers/ProductsController.cs
+++ API/Controllers/ProductsController.cs
@@ -1,4 +1,7 @@
-﻿using Microsoft.AspNetCore.Mvc;
+﻿using Core.Entities;
+using Infrastructure.Data;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
 
 namespace API.Controllers
 {
@@ -6,16 +9,24 @@ namespace API.Controllers
     [Route("api/[controller]")]
     public class ProductsController : Controller
     {
-        [HttpGet]
-        public string GetProducts()
+        private readonly StoreContext context;
+
+        public ProductsController(StoreContext context)
         {
-            return "my list of products";
+            this.context = context;
+        }
+
+        [HttpGet]
+        public async Task<ActionResult<List<Product>>> GetProducts()
+        {
+            return await context.Products.ToListAsync();
         }
 
         [HttpGet("{id}")]
-        public string GetProduct(int id)
+        public async Task<ActionResult<Product>> GetProduct(int id)
         {
-            return "my product";
+            return await context.Products.FindAsync(id);
+
         }
     }
 }
